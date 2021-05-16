8189e2f57eb8f699e435d4d9e491d7ad5dad934e
Toroi0610 <toroi0610@gmail.com>
2021-05-16 20:29:18 +0900

    fix update temp_field

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import yaml
 
 from unet_part import UNet, PCL
 
-from utils import convert_array_to_tensor, convert_tensor_to_array, getflow, get2orderderivative
+from utils import convert_array_to_tensor, convert_tensor_to_array
 
 with open("config.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
@@ -16,11 +16,15 @@ x_h = config["simulation_params"]["x_h"]
 y_h = config["simulation_params"]["y_h"]
 dx = config["simulation_params"]["dx"]
 dy = config["simulation_params"]["dy"]
+T_0 = config["simulation_params"]["T_0"]
 
-init_temp = np.random.randint(0, 80, size=[int(x_h/dx), int(y_h/dy)]).astype(np.float32)
+init_temp = np.random.random(size=[int(x_h/dx), int(y_h/dy)]).astype(np.float32) * T_0
 
 # set bounary condition
-init_temp[:, -1] = config["simulation_params"]["T_0"]
+init_temp[:, -1] = T_0
+init_temp[:, 0] = 0
+init_temp[0] = 0
+init_temp[-1] = 0
 
 temp_field = init_temp
 
